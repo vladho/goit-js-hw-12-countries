927bf91d69d384f3da515a670f54bd5ae16f01e7
Vlad <vladhopilo@gmail.com>
2021-01-26 23:16:23 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import './styles.css';
+import itemTpl from './handelbars/item.hbs';
 
+// fetch("https://restcountries.eu/rest/v2/name").then(responce => responce.json).then(console.log)
+// fetch('https://restcountries.eu/rest/v2/all?fields=name')
+//   .then(responce => responce.json())
+//   .then(console.log);
 
-fetch("https://restcountries.eu/rest/v2/name").then(responce => responce.json).then(console.log)
+const countryRef = document.querySelector('.input-country');
+const resultRef = document.querySelector('.result');
+
+countryRef.addEventListener('change', () => {
+  const acquiredInfo = fetch(
+    `https://restcountries.eu/rest/v2/name/${countryRef.value}`,
+  ).then(responce =>
+    responce.json().then(item => {
+      console.log(item);
+      const markup = itemTpl(item);
+      console.log(markup);
+      resultRef.insertAdjacentHTML('beforeend', markup);
+    }),
+  );
+});

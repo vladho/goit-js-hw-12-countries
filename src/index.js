f93a72d9556392b6b476d8a8263e16ec2979ff16
vladho <vladhopilo@gmail.com>
2021-01-27 14:20:45 +0200

    add pnotify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './styles.css';
 import itemTpl from './handelbars/item.hbs';
+import {  success } from '../node_modules/@pnotify/core/dist/PNotify.js';
+// import { alert, defaultModules } from '../node_modules/'
+//   import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import 'material-design-icons/iconfont/material-icons.css';
 
 // fetch("https://restcountries.eu/rest/v2/name").then(responce => responce.json).then(console.log)
 // fetch('https://restcountries.eu/rest/v2/all?fields=name')
@@ -10,6 +16,14 @@ const countryRef = document.querySelector('.input-country');
 const resultRef = document.querySelector('.result');
 
 countryRef.addEventListener('change', () => {
+    success({
+  title: 'Sticky Success',
+        text: 'Sticky success... I\'m not even gonna make a joke.',
+  delay: 3000,
+        hide: true,
+  closerHover: false,
+  stickerHover: false
+});
   const acquiredInfo = fetch(
     `https://restcountries.eu/rest/v2/name/${countryRef.value}`,
   ).then(responce =>
@@ -17,7 +31,8 @@ countryRef.addEventListener('change', () => {
       console.log(item);
       const markup = itemTpl(item);
       console.log(markup);
-      resultRef.insertAdjacentHTML('beforeend', markup);
+        resultRef.insertAdjacentHTML('beforeend', markup);
+        
     }),
   );
 });
